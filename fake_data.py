6f6ae6582774ad84ed93871862f199abe363d7fe
Braden McLain <bradenm@mopine.com>
2021-11-24 17:05:13 -0500

    first commit

---
+++ fake_data.py
@@ -0,0 +1,20 @@
+import cherrypy
+import numpy
+from gpiozero import LED
+
+red = 
+
+class demoExample:
+    @cherrypy.expose
+    def index(self):
+        return repr(numpy.random.random())
+    @cherrypy.expose
+    def led(self,state):
+        if state=="1":
+            print("do the things")
+        elif state=="0":
+            print("do not the things")
+    index.exposed = True
+cherrypy.config.update({'server.socket_port': 8099})
+cherrypy.engine.restart()
+cherrypy.quickstart(demoExample())
